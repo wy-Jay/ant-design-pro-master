9acae86badf59742ab87d5c99ee1ccf759149921
廉明v <lianming@ywwl.comv>
2018-07-16 10:42:26 +0800

    first commit

---
+++ src/layouts/UserLayout.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react';
+import { Link, Redirect, Switch, Route } from 'dva/router';
+import DocumentTitle from 'react-document-title';
+import { Icon } from 'antd';
+import GlobalFooter from '../components/GlobalFooter';
+import styles from './UserLayout.less';
+import logo from '../assets/logo.svg';
+import { getRoutes, getPageQuery, getQueryPath } from '../utils/utils';
+
+function getLoginPathWithRedirectPath() {
+  const params = getPageQuery();
+  const { redirect } = params;
+  return getQueryPath('/user/login', {
+    redirect,
+  });
+}
+
+class UserLayout extends React.PureComponent {
+  getPageTitle() {
+    const { routerData, location } = this.props;
+    const { pathname } = location;
+    let title = 'Ant Design Pro';
+    if (routerData[pathname] && routerData[pathname].name) {
+      title = `${routerData[pathname].name} - Ant Design Pro`;
+    }
+    return title;
+  }
+
+  render() {
+    const { routerData, match } = this.props;
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.top}>
+              <div className={styles.header}>
+                <Link to="/">
+                  <img alt="logo" className={styles.logo} src={logo} />
+                  <span className={styles.title}>项目名称</span>
+                </Link>
+              </div>
+              <div className={styles.desc}>项目描述</div>
+            </div>
+            <Switch>
+              {getRoutes(match.path, routerData).map(item => (
+                <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              ))}
+              <Redirect from="/user" to={getLoginPathWithRedirectPath()} />
+            </Switch>
+          </div>
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default UserLayout;
